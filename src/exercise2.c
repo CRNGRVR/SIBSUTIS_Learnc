a9871e27a1e6e09f8ebc9e2ca0e5063bd47e7a2b
Ivan <i.a.konuhov@gmail.com>
2025-10-13 20:25:56 +0700

    Lab5: Green

--- src/exercise2.c
+++ src/exercise2.c
@@ -2,14 +2,14 @@
 
 int main(int argc, char** argv)
 {
-    int numbers[10] = {};
-    scanf("%d %d %d %d %d %d %d %d %d %d", &numbers[0], &numbers[1], &numbers[2],
+    float numbers[10] = {};
+    scanf("%f %f %f %f %f %f %f %f %f %f", &numbers[0], &numbers[1], &numbers[2],
           &numbers[3], &numbers[4], &numbers[5], &numbers[6], &numbers[7],
           &numbers[8], &numbers[9]);
 
     for (int i = 9; i >= 0; i--)
     {
-        printf("%d ", numbers[i]);
+        printf("%f ", numbers[i]);
     }
 
     printf("\n");
--- src/exercise4.c
+++ src/exercise4.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char** argv) {
-    printf("Hello World!");
-    return 0;
+int main(int argc, char** argv)
+{
+    float numbers[10] = {};
+    scanf("%f %f %f %f %f %f %f %f %f %f", &numbers[0], &numbers[1], &numbers[2],
+          &numbers[3], &numbers[4], &numbers[5], &numbers[6], &numbers[7],
+          &numbers[8], &numbers[9]);
+
+    float x = atof(argv[1]);
+    int k = atoi(argv[2]);
+    
+    for (int i = 9; i > k; i--)
+    {
+        numbers[i] = numbers[i - 1];
+    }
+
+    numbers[k] = x;
+
+    for (int i = 0; i <= 9; i++)
+    {
+        printf("%f ", numbers[i]);
+    }
 }
